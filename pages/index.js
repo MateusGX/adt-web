cce0e600b13c5b2d8e412b895744168e4b1079d7
MateusGX <mateusam.ofc@outlook.com>
2021-07-21 02:34:58 -0300

    SSR

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,52 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import History from '../components/history'
-import useSWR from 'swr'
-import Link from 'next/link'
-export default function Home() {
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import History from "../components/history";
+import Link from "next/link";
+import absoluteUrl from "next-absolute-url";
 
-  const { data, error } = useSWR('/api/stories', (...args) => {
-    return fetch(...args).then(res => res.json())
-  })
-
+export default function Home({ data }) {
   return (
     <div className={styles.container}>
       <Head>
         <title>Aratuba do Terror - As histórias de uma pequena cidade</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className={styles.header}>
-
-      </header>
+      <header className={styles.header}></header>
       <main className={styles.main}>
         <div className={styles.title}>
           <h1>Aratuba do Terror</h1>
           <h4>As histórias de uma pequena cidade</h4>
         </div>
         <section className={styles.sectionHist}>
-          {
-            (data) ? (
-              data.map((item) => {
-                return <History title={item.title} key={item._id} excerpt={[item.excerpt]} slug={item._id} />
-              })
-            ) : (<></>)
-          }
+          {data && Array.isArray(data) ? (
+            data.map((item) => {
+              return (
+                <History
+                  title={item.title}
+                  key={item._id}
+                  excerpt={[item.excerpt]}
+                  slug={item._id}
+                />
+              );
+            })
+          ) : (
+            <></>
+          )}
         </section>
       </main>
 
       <footer className={styles.footer}>
-        <Link href="https://mateus-am.web.app/"><a>Copyright © 2021 Aratuba do Terror</a></Link>
+        <Link href="https://mateus-am.web.app/">
+          <a>Copyright © 2021 Aratuba do Terror</a>
+        </Link>
       </footer>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps({ req }) {
+  const { protocol, host } = absoluteUrl(req);
+  const res = await fetch(`${protocol}//${host}/api/stories`);
+  const data = await res.json();
+  return { props: { data } };
 }
